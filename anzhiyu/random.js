9f53da3336fd8fbee3c73d8e181f0cb68ca63a05
zbh <1658938409@qq.com>
2024-10-11 01:12:33 +0800

    Site updated: 2024-10-11 01:12:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/10/hello-world/"];function toRandomPost(){
+var posts=["2024/10/10/hello-world/","2024/10/11/vscode常用快捷键/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
